f5e1805ba73e3d68ca885cd5b51e7ef274a1b946
Ali Sokmen <58389668+sokmena@users.noreply.github.com>
2022-11-23 16:44:01 +0300

    Added the main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './content/home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './content/about/about.component';
+import { CvComponent } from './content/cv/cv.component';
+import { PortfolioComponent } from './content/portfolio/portfolio.component';
+import { GuitarComponent } from './content/guitar/guitar.component';
+import { ArcheryComponent } from './content/archery/archery.component';
+import { ContactComponent } from './content/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    CvComponent,
+    PortfolioComponent,
+    GuitarComponent,
+    ArcheryComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/content/archery/archery.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-archery',
+  templateUrl: './archery.component.html',
+  styleUrls: ['./archery.component.css']
+})
+export class ArcheryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
