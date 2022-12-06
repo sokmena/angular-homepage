eebba6464e1a79696204082774f480e6b7f459a9
Ali Sokmen <58389668+sokmena@users.noreply.github.com>
2022-12-06 21:52:02 +0300

    Set up routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './content/about/about.component';
+import { ArcheryComponent } from './content/archery/archery.component';
+import { ContactComponent } from './content/contact/contact.component';
+import { CvComponent } from './content/cv/cv.component';
+import { GuitarComponent } from './content/guitar/guitar.component';
+import { HomeComponent } from './content/home/home.component';
+import { PortfolioComponent } from './content/portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'archery', component: ArcheryComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'cv', component: CvComponent},
+  {path: 'guitar', component: GuitarComponent},
+  {path: 'portfolio', component: PortfolioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+}
